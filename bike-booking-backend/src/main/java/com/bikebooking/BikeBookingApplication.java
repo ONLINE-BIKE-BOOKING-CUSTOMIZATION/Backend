e179a5b65e71e0e033a6cfb14304ef681f19b576
sanket <sanketsawant784@gmail.com>
2026-01-27 14:31:09 +0530

    project intialisation/creation

---
+++ bike-booking-backend/src/main/java/com/bikebooking/BikeBookingApplication.java
@@ -0,0 +1,13 @@
+package com.bikebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeBookingApplication.class, args);
+	}
+
+}
---
+++ bike-booking-backend/src/test/java/com/bikebooking/BikeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
